d83e42079e93a2c26ce1baabe925b1cda70e43e3
Gussunjaya <gussunn14@gmail.com>
2026-05-26 23:38:30 +0800

    feat: configure Vue Router with basic views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import { RouterView } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView,
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView,
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-2xl text-primary">Dashboard Tanaman</h1>
+  </div>
+</template>
